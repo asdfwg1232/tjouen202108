a334c2940c4521a9ce72423dfd899f4a17aeaf2b
rodio <rodio@LAPTOP-H06GTU96>
2021-10-04 16:36:28 +0900

    student 1명추가

--- git_test/src/my/git/test/Test.java
+++ git_test/src/my/git/test/Test.java
@@ -4,6 +4,9 @@ public class Test {
 	public static void main(String[] args) {
 		Student st = new Student("dd", 10);
 		System.out.println(st);
+		
+		Student st2 = new Student("gw", 10);
+		System.out.println(st2);
 	}
 
 }
